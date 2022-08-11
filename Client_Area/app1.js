ec5cea610828a076020e3b59dee7eb943529f8f5
Khatarnaak Kaveer <86919827+Kaveer2009@users.noreply.github.com>
2022-08-11 21:34:42 +0530

    hi

--- Client_Area/app1.js
+++ Client_Area/app1.js
@@ -1,3 +1,20 @@
+// firebase links starts
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCZBWnYlII7deGcr8auHxi6ukMbuKki0i0",
+    authDomain: "the-big-fish-b3445.firebaseapp.com",
+    databaseURL: "https://the-big-fish-b3445-default-rtdb.firebaseio.com",
+    projectId: "the-big-fish-b3445",
+    storageBucket: "the-big-fish-b3445.appspot.com",
+    messagingSenderId: "44348757949",
+    appId: "1:44348757949:web:cf2a3df67b1b4a6060e4d4"
+  };
+  
+  // Initialize Firebase
+  // const app = initializeApp(firebaseConfig);
+  firebase.initializeApp(firebaseConfig);
+// firebase links ends
+
 var code = "staff_of_tbf";
 
 
@@ -5,9 +22,52 @@ function check(){
     if(document.getElementById("code_input").value === code){
         document.getElementById("h3_success").innerHTML = "The code is correct.<br>Access granted.";
         document.getElementById("h3_fail").innerHTML = ""
-        window.location="https://the-big-fish-b3445-default-rtdb.firebaseio.com";
+        // window.location="https://the-big-fish-b3445-default-rtdb.firebaseio.com";
+        window.location="client.html"
     }
     else{
         document.getElementById("h3_fail").innerHTML = "The code is incorrect.<br>Access denied."
     }
 }
+
+function addOrder(){
+    var ordername = document.getElementById("order_name").value;
+    
+    firebase.database().ref("/").child(ordername).update({
+          purpose:"adding order manually"
+    });
+
+    localStorage.setItem("ordername", ordername);
+    window.location = "check_order.html";
+  }
+
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+    order_names = childKey;
+   //Start code
+   console.log("order - " + order_names);
+   console.log(order_names);
+
+
+   row = "<div class='order_name' id=" + order_names + " onclick='redirecttoordername(this.id)'>" + order_names + "</div><hr>";
+   document.getElementById("output").innerHTML += row; 
+
+
+   //End code
+   });});}
+
+getData();
+
+function redirecttoordername(name){
+    console.log(name);
+    localStorage.setItem("ordername", name);
+    window.location="check_order.html";
+}
+
+
+
+
+
+
+
+
---
+++ Client_Area/check.js
@@ -0,0 +1,48 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCZBWnYlII7deGcr8auHxi6ukMbuKki0i0",
+    authDomain: "the-big-fish-b3445.firebaseapp.com",
+    databaseURL: "https://the-big-fish-b3445-default-rtdb.firebaseio.com",
+    projectId: "the-big-fish-b3445",
+    storageBucket: "the-big-fish-b3445.appspot.com",
+    messagingSenderId: "44348757949",
+    appId: "1:44348757949:web:cf2a3df67b1b4a6060e4d4"
+  };
+  
+  // Initialize Firebase
+  // const app = initializeApp(firebaseConfig);
+  firebase.initializeApp(firebaseConfig);
+// firebase links ends
+
+var ordername = localStorage.getItem("ordername");
+
+document.getElementById("heading").innerHTML = ordername;
+
+function getData() { firebase.database().ref("/"+ordername).on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
+    firebase_message_id = childKey;
+    order_data = childData;
+
+    uname = order_data["name"];
+    ucontact = order_data["phone_no"];
+    uemail = order_data["email"];
+    uaddress = order_data["address"];
+    uorder = order_data["order"];
+    uquantity = order_data["quantity"];
+
+    
+    row1 = "<div class='container' id='inf'>  <div class='row' id='inf_row'>   <div class='col'>Name : </div> <div class='col'>"+uname+"</div>";
+    row2 = "<div class='container' id='inf'>  <div class='row' id='inf_row'>   <div class='col'>Contact : </div> <div class='col'>+91 "+ucontact+"</div>";
+    row3 = "<div class='container' id='inf'>  <div class='row' id='inf_row'>   <div class='col'>E - Mail : </div> <div class='col'>"+uemail+"</div>";
+    row4 = "<div class='container' id='inf'>  <div class='row' id='inf_row'>   <div class='col'>Address : </div> <div class='col'>"+uaddress+"</div>";
+    row5 = "<div class='container' id='inf'>  <div class='row' id='inf_row'>   <div class='col'>Order : </div> <div class='col'>"+uorder+"</div>";
+    row6 = "<div class='container' id='inf'>  <div class='row' id='inf_row'>   <div class='col'>Quantity : </div> <div class='col'>"+uquantity+"</div>";
+
+    document.getElementById("output").innerHTML = row1 + row2 + row3 + row4 + row5 + row6;
+
+    console.log(firebase_message_id);
+    console.log(order_data);
+//start code
+
+//End code
+ } });  }); }
+
+ getData();
